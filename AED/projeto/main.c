12ca20257d630ba9e629decec8392b3fad014075
Dic0 <dwdieric@gmail.com>
2023-08-30 19:29:48 -0300

    chaves organizadas

--- AED/projeto/main.c
+++ AED/projeto/main.c
@@ -2,16 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct _jogador
-{
+typedef struct _jogador {
     char nome[40];
     char posicao[2];
     float altura;
     int numCamisa, idade, titulosCarreira;
 } Jogador;
 
-typedef struct _timeBasquete
-{
+typedef struct _timeBasquete {
     char nome[40];
     char tecnico[40];
     char conferencia[40];
@@ -19,15 +17,13 @@ typedef struct _timeBasquete
     Jogador jogadores[100];
 } TimeBasquete;
 
-TimeBasquete cadastrarTime(TimeBasquete basquete[], int numTimes)
-{
+TimeBasquete cadastrarTime(TimeBasquete basquete[], int numTimes) {
 
     TimeBasquete novoTime;
     novoTime.numJogadores = 0;
     char sair = 'n';
 
-    if (numTimes == 0)
-    {
+    if (numTimes == 0) {
         printf("\nEscreva o nome do Time:");
         scanf(" %[^\n]s", novoTime.nome);
         printf("\nEscreva o nome do Tecnico do Time:");
@@ -40,32 +36,24 @@ TimeBasquete cadastrarTime(TimeBasquete basquete[], int numTimes)
         scanf("%d", &novoTime.anoFundacao);
         printf("\n");
     }
-    else
-    {
-        do
-        {
+    else {
+        do {
             printf("\nEscreva o nome do Time:");
             scanf(" %[^\n]s", novoTime.nome);
 
             int i, nomeExistente = 0;
 
-            for (i = 0; i < numTimes; i++)
-            {
-                if (strcasecmp(novoTime.nome, basquete[i].nome) == 0)
-                {
+            for (i = 0; i < numTimes; i++) {
+                if (strcasecmp(novoTime.nome, basquete[i].nome) == 0) {
                     printf("\nEsse nome de time ja existe!\n");
                     nomeExistente = 1; // Nome correspondente encontrado
                     break;             // Saia do loop, já que encontramos um nome igual
                 }
             }
             if (!nomeExistente)
-            {
                 sair = 'n'; // Define sair como 'n' apenas se nenhum nome correspondente foi encontrado
-            }
             else
-            {
                 sair = 's';
-            }
 
         } while (sair == 's');
 
@@ -82,41 +70,32 @@ TimeBasquete cadastrarTime(TimeBasquete basquete[], int numTimes)
     return novoTime;
 }
 
-void listarTimes(TimeBasquete basquete[], int numTimes, int opcao)
-{
-    if (numTimes == 0 && opcao == 2)
-    {
+void listarTimes(TimeBasquete basquete[], int numTimes, int opcao) {
+    if (numTimes == 0 && opcao == 2) {
         printf("\nNenhum time foi cadastrado\n\n");
     }
-    else
-    {
+    else {
         int i;
-        for (i = 0; i < numTimes; i++)
-        {
+        for (i = 0; i < numTimes; i++) {
             printf("\nNome do Time %d: - %s\n", i + 1, basquete[i].nome);
         }
     }
 }
 
-void editarTime(TimeBasquete basquete[], int numTimes)
-{
+void editarTime(TimeBasquete basquete[], int numTimes) {
 
-    if (numTimes == 0)
-    {
+    if (numTimes == 0) {
         printf("\nNenhum time foi cadastrado para realizar as edicoes\n\n");
     }
-    else
-    {
+    else {
         char nomeTimes[50];
         int i, opcao;
 
         printf("\nEscreva o nome do Time que deseja editar: \n");
         scanf(" %[^\n]s", nomeTimes);
 
-        for (i = 0; i < numTimes; i++)
-        {
-            if (strcasecmp(nomeTimes, basquete[i].nome) == 0)
-            {
+        for (i = 0; i < numTimes; i++) {
+            if (strcasecmp(nomeTimes, basquete[i].nome) == 0) {
                 // compara duas strings
                 printf("\n1 - Para editar o nome do Time: ");
                 printf("\n2 - Para editar o tecnico do Time: ");
@@ -126,8 +105,7 @@ void editarTime(TimeBasquete basquete[], int numTimes)
                 printf("\n\nSelecione uma opcao :");
                 scanf("%d", &opcao);
 
-                switch (opcao)
-                {
+                switch (opcao) {
                 case 1:
                     printf("\n\nDigite o novo nome do Time: ");
                     scanf(" %[^\n]s", basquete[i].nome);
@@ -160,57 +138,46 @@ void editarTime(TimeBasquete basquete[], int numTimes)
     }
 }
 
-void removerTime(TimeBasquete basquete[], int numTimes)
-{
-    if (numTimes == 0)
-    {
+void removerTime(TimeBasquete basquete[], int numTimes) {
+    if (numTimes == 0) {
         printf("\nNenhum time foi cadastrado para realizar a remocao\n\n");
     }
-    else
-    {
+    else {
         char nomeTime[50];
         int i, encontrei = 0;
 
         printf("\nEscreva o nome do Time que deseja remover: \n");
         scanf(" %[^\n]s", nomeTime);
 
-        for (i = 0; i < numTimes; i++)
-        {
-            if (strcasecmp(nomeTime, basquete[i].nome) == 0)
-            {
+        for (i = 0; i < numTimes; i++) {
+            if (strcasecmp(nomeTime, basquete[i].nome) == 0) {
                 encontrei = 1;
             }
-            if (encontrei)
-            {
+            if (encontrei) {
                 if (i < numTimes - 1) // verifica se existe algo na proxima posicao do vetor
                     basquete[i] = basquete[i + 1];
             }
         }
-        if (encontrei)
-        {
+        if (encontrei) {
             printf("\nOperacao realizada com sucesso!\n\n");
         }
-        else
-        {
+        else {
             printf("\nTime nao encontrado!\n\n");
         }
     }
 }
 
-Jogador cadastrarJogador(Jogador jogadores[], int numJogadores)
-{
+Jogador cadastrarJogador(Jogador jogadores[], int numJogadores) {
     Jogador novoJodador;
     char sair = 'n';
     int opcao;
 
-    if (numJogadores == 0)
-    {
+    if (numJogadores == 0) {
         printf("\nEscreva o nome do Jogador:");
         scanf(" %[^\n]s", novoJodador.nome);
         printf("Escreva a altura do Jogador:");
         scanf("%f", &novoJodador.altura);
-        do
-        {
+        do {
 
             printf("Digite o numero da camisa do jogador: ");
             scanf("%3d", &novoJodador.numCamisa);
@@ -233,8 +200,7 @@ Jogador cadastrarJogador(Jogador jogadores[], int numJogadores)
         printf("5 - Pivo (C):\n");
         scanf("%d", &opcao);
 
-        switch (opcao)
-        {
+        switch (opcao) {
         case 1:
             strcpy(novoJodador.posicao, "G");
             break;
@@ -255,45 +221,37 @@ Jogador cadastrarJogador(Jogador jogadores[], int numJogadores)
             break;
         }
     }
-    else
-    {
+    else {
 
         printf("\nEscreva o nome do Jogador:");
         scanf(" %[^\n]s", novoJodador.nome);
         printf("Escreva a altura do Jogador:");
         scanf("%f", &novoJodador.altura);
 
-        do
-        {
-            do
-            {
+        do {
+            do {
 
                 printf("Digite o numero da camisa do jogador: ");
                 scanf("%3d", &novoJodador.numCamisa);
 
-                if (novoJodador.numCamisa < 10 || novoJodador.numCamisa >= 100)
-                {
+                if (novoJodador.numCamisa < 10 || novoJodador.numCamisa >= 100) {
                     printf("Digite 2 digitos!\n");
                 }
             } while (novoJodador.numCamisa < 10 || novoJodador.numCamisa >= 100);
 
             int i, numeroExistente = 0;
 
-            for (i = 0; i < numJogadores; i++)
-            {
-                if (novoJodador.numCamisa == jogadores[i].numCamisa)
-                {
+            for (i = 0; i < numJogadores; i++) {
+                if (novoJodador.numCamisa == jogadores[i].numCamisa) {
                     printf("\nJa existe um jogador com esse numero!\n\n");
                     numeroExistente = 1;
                     break;
                 }
             }
-            if (!numeroExistente)
-            {
+            if (!numeroExistente) {
                 sair = 'n';
             }
-            else
-            {
+            else {
                 sair = 's';
             }
 
@@ -311,8 +269,7 @@ Jogador cadastrarJogador(Jogador jogadores[], int numJogadores)
         printf("5 - Pivo (C):\n");
         scanf("%d", &opcao);
 
-        switch (opcao)
-        {
+        switch (opcao) {
         case 1:
             strcpy(novoJodador.posicao, "G");
             break;
@@ -337,41 +294,32 @@ Jogador cadastrarJogador(Jogador jogadores[], int numJogadores)
     return novoJodador;
 }
 
-void listarJogador(Jogador jogadores[], int numJogadores, int opcaoJogador)
-{
+void listarJogador(Jogador jogadores[], int numJogadores, int opcaoJogador) {
 
-    if (numJogadores == 0 && opcaoJogador == 2)
-    {
+    if (numJogadores == 0 && opcaoJogador == 2) {
         printf("\nNenhum jogador foi cadastrado\n\n");
     }
-    else
-    {
+    else {
         int i;
-        for (i = 0; i < numJogadores; i++)
-        {
+        for (i = 0; i < numJogadores; i++) {
             printf("\nNome do Jogador %d: - %s\n", i + 1, jogadores[i].nome);
         }
     }
 }
 
-void editarJogador(Jogador jogadores[], int numJogadores)
-{
-    if (numJogadores == 0)
-    {
+void editarJogador(Jogador jogadores[], int numJogadores) {
+    if (numJogadores == 0) {
         printf("\nNenhum jogador foi cadastrado para realizar as edicoes\n\n");
     }
-    else
-    {
+    else {
         char nomeJogadores[50];
         int i, opcao, opcaoPosicao;
 
         printf("\nEscreva o nome do Jogador que deseja editar: \n");
         scanf(" %[^\n]s", nomeJogadores);
 
-        for (i = 0; i < numJogadores; i++)
-        {
-            if (strcasecmp(nomeJogadores, jogadores[i].nome) == 0)
-            {
+        for (i = 0; i < numJogadores; i++) {
+            if (strcasecmp(nomeJogadores, jogadores[i].nome) == 0) {
                 // compara duas strings
                 printf("\n1 - Para editar o nome do Jogador: ");
                 printf("\n2 - Para editar a altura do Jogador: ");
@@ -382,8 +330,7 @@ void editarJogador(Jogador jogadores[], int numJogadores)
                 printf("\n\nSelecione uma opcao: ");
                 scanf("%d", &opcao);
 
-                switch (opcao)
-                {
+                switch (opcao) {
                 case 1:
                     printf("\nDigite o novo nome do Jogador: ");
                     scanf(" %[^\n]s", jogadores[i].nome);
@@ -393,8 +340,7 @@ void editarJogador(Jogador jogadores[], int numJogadores)
                     scanf("%f", &jogadores[i].altura);
                     break;
                 case 3:
-                    do
-                    {
+                    do {
 
                         printf("Digite o novo numero da camisa do jogador: ");
                         scanf("%3d", &jogadores[i].numCamisa);
@@ -427,8 +373,7 @@ void editarJogador(Jogador jogadores[], int numJogadores)
                     printf("5 - Pivo (C):");
                     scanf("%d", &opcaoPosicao);
 
-                    switch (opcaoPosicao)
-                    {
+                    switch (opcaoPosicao) {
                     case 1:
                         strcpy(jogadores[i].posicao, "G");
                         break;
@@ -461,50 +406,40 @@ void editarJogador(Jogador jogadores[], int numJogadores)
     }
 }
 
-void removerJogador(Jogador jogadores[], int numJogador)
-{
-    if (numJogador == 0)
-    {
+void removerJogador(Jogador jogadores[], int numJogador) {
+    if (numJogador == 0) {
         printf("\nNenhum jogador foi cadastrado para realizar a remocao\n\n");
     }
-    else
-    {
+    else {
         char nomeJogadores[50];
         int i, achei = 0;
 
         printf("\nEscreva o nome do jogador que deseja remover: \n");
         scanf(" %[^\n]s", nomeJogadores);
 
-        for (i = 0; i < numJogador; i++)
-        {
-            if (strcasecmp(nomeJogadores, jogadores[i].nome) == 0)
-            {
+        for (i = 0; i < numJogador; i++) {
+            if (strcasecmp(nomeJogadores, jogadores[i].nome) == 0) {
                 achei = 1;
             }
-            if (achei)
-            {
+            if (achei) {
                 if (i < numJogador - 1) // verifica se existe algo na proxima posicao do vetor
                     jogadores[i] = jogadores[i + 1];
             }
         }
-        if (achei)
-        {
+        if (achei) {
             printf("\nOperacao realizada com sucesso!\n\n");
         }
-        else
-        {
+        else {
             printf("\nJogador nao encontrado!\n\n");
         }
     }
 }
 
-int verificarPosicao(TimeBasquete basquete)
-{
+int verificarPosicao(TimeBasquete basquete) {
 
     int pg = 0, sg = 0, sf = 0, pf = 0, c = 0, i;
 
-    for (i = 0; i < basquete.numJogadores; i++)
-    {
+    for (i = 0; i < basquete.numJogadores; i++) {
         if (strcmp(basquete.jogadores[i].posicao, "G") == 0)
             pg++;
         else if (strcmp(basquete.jogadores[i].posicao, "SG") == 0)
@@ -519,20 +454,17 @@ int verificarPosicao(TimeBasquete basquete)
     return (pg >= 1 && sg >= 1 && sf >= 1 && pf >= 1 && c >= 1);
 }
 
-void quadra(TimeBasquete basquete)
-{
+void quadra(TimeBasquete basquete) {
 
     int i, a = 0, b = 0, c = 0, d = 0, e = 0;
     char nomeA[50], nomeB[50], nomeC[50], nomeD[50], nomeE[50];
 
-    for (i = 0; i < basquete.numJogadores; i++)
-    {
+    for (i = 0; i < basquete.numJogadores; i++) {
         // Obter a posição do jogador na quadra
         char posicao = basquete.jogadores[i].posicao[0];
 
         // Exibir o número da camiseta do jogador na posição correspondent'e na quadra
-        switch (posicao)
-        {
+        switch (posicao) {
         case 'G':
             a = basquete.jogadores[i].numCamisa;
             strcpy(nomeA, basquete.jogadores[i].nome);
@@ -594,15 +526,13 @@ void quadra(TimeBasquete basquete)
     printf("\n%d - %s", e, nomeE);
 }
 
-int main()
-{
+int main() {
     TimeBasquete basquete[100];
     int numTimes = 0;
     int opcao, selecionarTime, opcaoJogador;
     char reiniciar, sair;
 
-    do
-    {
+    do {
         printf("\n\n\tMENU\n");
         printf("\n1 - Cadastrar um novo time de Basquete:");
         printf("\n2 - Listar todos os times de Basquete:");
@@ -615,8 +545,7 @@ int main()
         scanf("%d", &opcao);
         printf("\n\n");
 
-        switch (opcao)
-        {
+        switch (opcao) {
         case 1:
             basquete[numTimes] = cadastrarTime(basquete, numTimes);
             numTimes++;
@@ -634,17 +563,14 @@ int main()
             numTimes--;
             break;
         case 5:
-            if (numTimes > 0)
-            {
+            if (numTimes > 0) {
                 listarTimes(basquete, numTimes, opcao);
                 printf("\n\n");
                 printf("Selecione o time no qual deseja criar, editar ou remover os jogadores:");
                 scanf("%d", &selecionarTime);
-                if (selecionarTime > 0 && selecionarTime <= numTimes)
-                {
+                if (selecionarTime > 0 && selecionarTime <= numTimes) {
 
-                    do
-                    {
+                    do {
                         printf("\n\n\tMENU\n");
                         printf("\n1 - Cadastrar um novo Jogador:");
                         printf("\n2 - Listar todos os Jogadores:");
@@ -655,8 +581,7 @@ int main()
                         scanf(" %d", &opcaoJogador);
                         printf("\n\n");
 
-                        switch (opcaoJogador)
-                        {
+                        switch (opcaoJogador) {
                         case 1:
                             basquete[selecionarTime - 1].jogadores[basquete[selecionarTime - 1].numJogadores] = cadastrarJogador(basquete[selecionarTime - 1].jogadores, basquete[selecionarTime - 1].numJogadores);
                             basquete[selecionarTime - 1].numJogadores++;
@@ -681,44 +606,36 @@ int main()
                             printf("\nOpcao inexistente!\n\n");
                         }
 
-                        if (opcaoJogador != 5)
-                        {
+                        if (opcaoJogador != 5) {
                             reiniciar = 'n';
                         }
 
                     } while (reiniciar == 'N' || reiniciar == 'n');
                     break;
                 }
-                else
-                {
+                else {
                     printf("\nOpcao inexistente!\n\n");
                 }
             }
-            else
-            {
+            else {
                 printf("Primeiro e necessario cadastrar um time!\n\n");
             }
 
         case 6:
-            if (numTimes > 0)
-            {
+            if (numTimes > 0) {
                 listarTimes(basquete, numTimes, opcao);
                 printf("\n\n");
                 printf("Selecione o time no qual deseja exibir em quadra:");
                 scanf("%d", &selecionarTime);
-                if (selecionarTime > 0 && selecionarTime <= numTimes)
-                {
-                    if (verificarPosicao(basquete[selecionarTime - 1]))
-                    {
+                if (selecionarTime > 0 && selecionarTime <= numTimes) {
+                    if (verificarPosicao(basquete[selecionarTime - 1])) {
                         quadra(basquete[selecionarTime - 1]);
                     }
-                    else
-                    {
+                    else {
                         printf("\nO time precisa ter pelo menos um jogador em cada posicao para exibir a quadra!\n\n");
                     }
                 }
-                else
-                {
+                else {
                     printf("\nOpcao inexistente!\n\n");
                 }
             }
@@ -731,8 +648,7 @@ int main()
             printf("\nOpcao Invalida!\n\n");
         }
 
-        if (opcao != 7)
-        {
+        if (opcao != 7) {
             sair = 'n';
         }
 
